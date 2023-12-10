765aaed737fbdfd49f14d152953d1e9a5e67bcae
Shahrukh A. Khan <iam@shaz3e.com>
2023-12-11 00:13:06 +0500

    Dashboard Header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,8 @@
 <template>
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Blank Page</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Blank Page</li>
-                        </ol>
-                    </div>
-                </div>
-            </div><!-- /.container-fluid -->
-        </section>
+
+        <DashboardHeader :pageTitle="pageTitle" />
 
         <!-- Main content -->
         <section class="content">
@@ -51,3 +37,18 @@
     </div>
     <!-- /.content-wrapper -->
 </template>
+
+<script>
+import DashboardHeader from '../components/DashboardHeader.vue'
+
+export default {
+    components: {
+        DashboardHeader
+    },
+    data(){
+        return{
+            pageTitle: 'Dashboard'
+        }
+    }
+}
+</script>
