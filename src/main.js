4e458bcc587df82a73abc42d523f255d87dbeee1
Shahrukh A. Khan <iam@shaz3e.com>
2023-12-11 00:04:02 +0500

    Blank Template

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// CSS
+import './assets/plugins/fontawesome-free/css/all.min.css'
+import './assets/dist/css/adminlte.min.css'
+import './assets/dist/js/adminlte.min.js'
+// JS
+import './assets/plugins/jquery/jquery.min.js'
+import './assets/plugins/bootstrap/js/bootstrap.bundle.min.js'
+import './assets/dist/js/adminlte.min.js'
 
 createApp(App).use(router).mount('#app')
